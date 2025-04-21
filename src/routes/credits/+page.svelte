142290b054c883d46ada308305db34b387a0141c
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2025-04-21 17:13:21 -0400

    splash and credits

--- src/routes/credits/+page.svelte
+++ src/routes/credits/+page.svelte
@@ -3,9 +3,14 @@
 </script>
 
 <div class="wrap">
-	<h1>Lucky Tacos</h1>
-	<a class="button" href="/play">Play</a>
-	<a class="button" href="/credits">Credits</a>
+	<h1>Credits</h1>
+	<h2>Created By</h2>
+	<div class="creditText">Dylan Myers</div>
+	<h2>Tested By</h2>
+	<div class="creditText">Dylan Myers</div>
+	<h2>Assets</h2>
+	<div class="creditText">Kenny.nl</div>
+	<a class="button" href="/">Back</a>
 </div>
 
 <style lang="scss">
@@ -19,16 +24,23 @@
 		display: flex;
 		flex-direction: column;
 		align-items: center;
-		justify-content: center;
+		justify-content: start;
 		background: beige;
 		gap: 0.5rem;
+		padding: 1rem;
 	}
 
 	h1 {
 		font-size: 4rem;
 	}
 
+	h2 {
+		margin: 0;
+		font-size: 2rem;
+	}
+
 	.button {
+		margin-top: 5rem;
 		display: flex;
 		padding-top: 1.5rem;
 		padding-bottom: 1rem;
@@ -39,5 +51,11 @@
 		align-items: center;
 		justify-content: center;
 		border-radius: 0.5rem;
+	}
+
+	.creditText {
+		font-family: sans-serif;
+		font-size: 1.5rem;
+		margin-bottom: 1rem;
 	}
 </style>
